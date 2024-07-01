19e43158c66379441cd88c2c341b55334419f535
RMDECLIIT <119407882+bharathwaj1002@users.noreply.github.com>
2024-07-01 12:45:09 +0530

    updated webhook urls

--- repoapp/urls.py
+++ repoapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('webhook/github/', views.github_webhook, name='github_webhook'),
+    path('webhook/', views.github_webhook, name='github_webhook'),
     path('pullrequests/', views.pull_requests_list, name='pull_requests_list'),
     path('repositories/', views.repositories_list, name='repositories_list'),
     
